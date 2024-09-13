59d49eb8ca4eceb711d4df87f07b6900ddbc3d2f
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-09-14 00:21:05 +0900

    Time: 0 ms (100%), Space: 47.7 MB (67.63%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int l = 0;
+        int r = citations.length - 1;
+        int h = 0;
+
+        while(l<=r) {
+            int mid = (l+r) / 2;
+            if(citations.length-mid <= citations[mid]) {
+                h = citations.length-mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return h;
+    }
+}
