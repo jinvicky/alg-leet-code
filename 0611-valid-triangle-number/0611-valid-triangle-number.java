098bc76981aca310bc27e30601efce1b768236f6
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-09-15 01:31:32 +0900

    Time: 23 ms (88.09%), Space: 43.4 MB (29.03%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -2,24 +2,40 @@ class Solution {
     public int triangleNumber(int[] nums) {
         Arrays.sort(nums); //일단 정렬하고 들어가
 
-        int res = 0;
-        int n = nums.length;
-        int i;
-        int j;
-        int k;
+        // int res = 0;
+        // int n = nums.length;
+        // int i;
+        // int j;
+        // int k;
 
-        for(i = 0; i < n; i++) {
-            for(j = i+1, k = j+1; j<(n-1) && k<=n;) {
-                if(k == n || nums[i] + nums[j] <= nums[k]) {
-                    if(k > (j+1)) {
-                        res += k - j - 1;
-                    }
-                    j++;
+        // for(i = 0; i < n; i++) {
+        //     for(j = i+1, k = j+1; j<(n-1) && k<=n;) {
+        //         if(k == n || nums[i] + nums[j] <= nums[k]) {
+        //             if(k > (j+1)) {
+        //                 res += k - j - 1;
+        //             }
+        //             j++;
+        //         } else {
+        //             k++;
+        //         }
+        //     }
+        // }
+        // return res;
+
+        int ans = 0;
+        for(int i = nums.length - 1; i >= 2; i--) {
+            int low = 0;
+            int high = i-1;
+
+            while(low < high) {
+                if(nums[low] + nums[high] > nums[i]) {
+                    ans += (high - low);
+                    high--;
                 } else {
-                    k++;
+                    low++;
                 }
             }
         }
-        return res;
+        return ans;
     }
 }
