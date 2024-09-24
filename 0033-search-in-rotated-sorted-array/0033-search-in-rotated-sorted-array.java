85594428b26ba2301d9b5bcb3540557512b49040
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-09-25 00:03:30 +0900

    Time: 0 ms (100%), Space: 41.8 MB (69.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,41 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int[] copy = nums.clone();
-        Arrays.sort(copy);
+        int left = 0;
+        int right = nums.length - 1;
 
-        int value = binarySearch(copy, target);
-        if(value == -1) {
-            return -1;
-        } else {
-            // 이제 이 value를 가지고 nums에서 몇번째 인덱스인지 찾아야 한다.
-            for(int i = 0; i < nums.length; i++) {
-                if(nums[i] == copy[value]) {
-                    return i;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[left]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
                 }
             }
         }
-        return -1;
-    }
 
-    public int binarySearch(int[] copy, int target) {
-        int start = 0;
-        int end = copy.length - 1;
-
-        while(start <= end) {
-            int mid = start + (end - start ) / 2;
-
-            if(target < copy[mid]) {
-                end = mid - 1;
-            }
-
-            if(target > copy[mid]) {
-                start = mid + 1;
-            }
-
-            if(target == copy[mid]) {
-                return mid;
-            }
-        }
-        return -1;
+        return -1;        
     }
 }
