f848e595df35967155cad317fc7b3158fecb1d8f
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-09-29 13:40:30 +0900

    Time: 1028 ms (5.25%), Space: 55.2 MB (50.1%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,30 +1,21 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        int[] prefixSums = new int[nums.length];
-        int zCnt = 0;
-        int fCnt = 0;
+        int N = nums.length;
+        int[] mp = new int[2*N+2];
+        int current = N;
+        int result = 0;
 
-
-        for(int i = 0; i < prefixSums.length; i++) {
-            int item = nums[i];
-            if(item == 0) {
-                zCnt++;
+        for(int i = 0; i < N; i++) {
+            current += (nums[i] << 1) - 1;
+            System.out.println(current);
+            if(current == N) {
+                result = i+1;
+            } else if (mp[current] == 0) {
+                mp[current] = i+1;
             } else {
-                fCnt++;
-            }
-
-            if(i == 0) {
-                prefixSums[i] = 0;
-                continue;
-            }
-
-            if(zCnt != 0 && fCnt != 0) {
-                prefixSums[i] = Math.min(zCnt, fCnt);
-            } else {
-                prefixSums[i] = prefixSums[i-1];
+                result = Math.max(result, i - mp[current] + 1);
             }
         }
-
-        return prefixSums[nums.length - 1] * 2;
+        return result;
     }
 }
