b1ba3f7d93a9fce9f108a68aca43e4f63485c966
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-01 16:26:59 +0900

    Time: 444 ms (22.65%), Space: 52.9 MB (12.21%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,26 @@
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       List<List<Integer>> res = new ArrayList<>();
-       Arrays.sort(nums);
+        Set<List<Integer>> resultSet = new HashSet<>();
+        Arrays.sort(nums); // 먼저 정렬합니다.
 
-       for(int i = 0; i < nums.length; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) {
-                continue;
-                // 0보다 크고 이전과 값이 같은 경우 스킵한다.
+        for (int i = 0; i < nums.length; i++) {
+            // 중복된 값 처리 (i > 0일 때만)
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // 같은 숫자를 건너뜁니다.
             }
 
-            int j = i+1;
-            int k = nums.length - 1;
-
-            while(j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if(total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-
-                    while(nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
+            Set<Integer> seen = new HashSet<>();
+            for (int j = i + 1; j < nums.length; j++) {
+                int complement = -nums[i] - nums[j];
+                if (seen.contains(complement)) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                    Collections.sort(triplet); // 정렬하여 리스트에 추가합니다.
+                    resultSet.add(triplet);
                 }
+                seen.add(nums[j]);
             }
-       }
-       return res;
+        }
+        return new ArrayList<>(resultSet);
     }
 }
