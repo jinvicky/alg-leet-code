a91fa6d5f4f5be088482155fe0f3e14050cb8fa4
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-02 15:37:29 +0900

    Time: 5 ms (53.1%), Space: 42.8 MB (90.85%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int charToNumber(char c) {
+        return c - 'a'; // 'a'는 0, 'b'는 1, ..., 'z'는 25
+    }   
+
+    public boolean isAnagram(String s, String t) {
+        int[] sAlphas = new int[26];
+        int[] tAlphas = new int[26];
+
+        // 각 문자열의 문자를 카운트
+        for (int i = 0; i < s.length(); i++) {
+            sAlphas[charToNumber(s.charAt(i))]++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            tAlphas[charToNumber(t.charAt(i))]++;
+        }
+
+        // 두 배열 비교
+        for (int j = 0; j < 26; j++) {
+            if (sAlphas[j] != tAlphas[j]) {
+                return false; // 개수가 다르면 애너그램이 아님
+            }
+        }
+
+        return true; // 모든 문자의 개수가 같으면 애너그램
+    }
+}
