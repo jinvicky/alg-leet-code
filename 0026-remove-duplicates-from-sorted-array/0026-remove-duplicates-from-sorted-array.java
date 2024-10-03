ecfc763a9e7fbaa0c0d8cab0e7866ace8982f806
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-03 13:04:08 +0900

    Time: 0 ms (100%), Space: 44.7 MB (63.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -8,15 +8,6 @@ class Solution {
                 insertIdx++;
             }
         }
-        // 개수를 리턴하려면 전체 배열 길이에서 insertIdx를 빼면 되지 않을까? 
-
-        System.out.println(insertIdx);
-
-        // if(insertIdx - 1 == nums.length) {
-        //     return nums.length - insertIdx + 1;
-        // }
         return nums.length - (nums.length - insertIdx);
     }
-
-
 }
