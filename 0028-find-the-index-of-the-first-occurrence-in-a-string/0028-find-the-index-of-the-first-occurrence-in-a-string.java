67b747c43b176da92bbce34708e0399fce46869f
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-03 14:03:38 +0900

    Time: 1 ms (27.55%), Space: 41.4 MB (48.02%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,17 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);   
+
+        if(haystack.length() < needle.length()) { // 일단 needle보다 작으면 안되겠지?
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int sliceRange = i + needle.length();
+            if(haystack.substring(i, sliceRange).equals(needle)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
