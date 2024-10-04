cf555b5a97f53a88f5c7af5e751eddca4093fada
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-04 10:02:14 +0900

    Time: 25 ms (73.16%), Space: 40.6 MB (18.68%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,10 +3,10 @@ public class Solution extends VersionControl {
         int low = 1;
         int high = n;
 
-        while (low < high) {
+        while (low <= high) {
             int mid = low + (high - low) / 2; // 중간값 계산
             if (isBadVersion(mid)) {
-                high = mid; // 잘못된 버전이므로, 범위를 왼쪽으로 줄임
+                high = mid - 1; // 잘못된 버전이므로, 범위를 왼쪽으로 줄임
             } else {
                 low = mid + 1; // 잘못된 버전이 아니므로, 범위를 오른쪽으로 줄임
             }
