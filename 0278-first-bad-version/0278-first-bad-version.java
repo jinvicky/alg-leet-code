52c7aafaae80697a8e1e50465cf76ac2c86ca31e
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-04 09:58:48 +0900

    Time: 24 ms (98.24%), Space: 40 MB (91.59%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -1,24 +1,17 @@
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
-
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int start = 0;
-        int end = n;
-        int index = n;
+        int low = 1;
+        int high = n;
 
-        while(start<=end) {
-            int mid = start + (end - start) / 2;
-            boolean isBad = isBadVersion(mid);
-
-            if(isBad) {
-                // 나쁘면 더 start에 가까운 값으로 넣는다.
-                index = mid;
-                end = mid - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2; // 중간값 계산
+            if (isBadVersion(mid)) {
+                high = mid; // 잘못된 버전이므로, 범위를 왼쪽으로 줄임
             } else {
-                start = mid + 1;
+                low = mid + 1; // 잘못된 버전이 아니므로, 범위를 오른쪽으로 줄임
             }
         }
-        return index;
+
+        return low; // low가 첫 번째 잘못된 버전
     }
 }
