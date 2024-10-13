3e278fb2af67892b8400fc75837ab1ff46bad226
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-13 13:06:30 +0900

    Time: 1 ms (86.63%), Space: 42.8 MB (8.34%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+
+        for(int i = 0; i < candies.length; i++) {
+            int item = candies[i];
+            max = Math.max(max, item);
+
+            candies[i] = item + extraCandies;
+        }
+
+        List<Boolean> ans = new ArrayList<>();
+        for(int j = 0; j < candies.length; j++) {
+            ans.add(candies[j] >= max ? true : false);
+        }
+        return ans;
+    }
+}
