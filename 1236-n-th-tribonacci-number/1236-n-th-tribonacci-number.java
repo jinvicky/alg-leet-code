42bcf6c7c8bbb0784e1864ed0cd456855bb5e818
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-14 18:34:33 +0900

    Time: 0 ms (100%), Space: 40.4 MB (38.64%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -9,11 +9,10 @@ class Solution {
        if (n== 1 || n==2) {
          return 1;
        }
-
+       dp[0] = 0;
        dp[1] = 1;
        dp[2] = 1;
-       dp[3] = 2;
-       for(int i = 4; i <= n; i++) {
+       for(int i = 3; i <= n; i++) {
             dp[i] = dp[i-1]  + dp[i-2] + dp[i-3];
        }
        return dp[n];
