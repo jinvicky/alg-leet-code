d32f3a1d0f4dc0a4030c749ed7ef5260e9d78a28
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-14 15:21:07 +0900

    Time: 21 ms (62.04%), Space: 54.5 MB (26.83%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.java
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.java
@@ -7,24 +7,13 @@ class RecentCounter {
     
     public int ping(int t) {
         list.add(t);
-        int cnt = 0;
-
         int rs = t - 3000;
         int re = t;
 
-        int flag = list.size();
-
-        while(flag > 0) {
-            int item = list.poll();
-            if(rs <= item && item <= re) {
-                cnt++;
-            }
-            if(rs <= item) {
-                list.add(item); // poll한 값을 다시 넣는다. 다만 rs 보다 작으면 안 넣어
-            }
-            flag--; // 루프 감소
+        while(list.peek() < rs) {
+            list.poll();
         }
-        return cnt;
+        return list.size();
     }
 }
 
