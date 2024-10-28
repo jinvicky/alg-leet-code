eed62a32a7b39d2fab35619334f172412325842b
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-28 11:02:41 +0900

    Time: 42 ms (10.77%), Space: 45.6 MB (37.88%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,27 +1,57 @@
+// class Solution {
+//     public String predictPartyVictory(String senate) {
+//         Queue<Integer> rad = new LinkedList<>();
+//         Queue<Integer> dir = new LinkedList<>();
+//         int n = senate.length();
+
+//         for(int i = 0; i < n; i++) {
+//             if(senate.charAt(i) == 'R') {
+//                 rad.add(i);
+//             } else {
+//                 dir.add(i);
+//             }
+//         }
+
+//         while(!rad.isEmpty() && !dir.isEmpty()) {
+//             if(rad.peek() < dir.peek()) {
+//                 rad.add(n++);
+//             } else {
+//                 dir.add(n++);
+//             }
+//             rad.poll();
+//             dir.poll();
+//         }
+
+//         return rad.isEmpty() ? "Dire" : "Radiant";
+//     }
+// }
+
 class Solution {
     public String predictPartyVictory(String senate) {
-        Queue<Integer> rad = new LinkedList<>();
-        Queue<Integer> dir = new LinkedList<>();
-        int n = senate.length();
+        Queue<Integer> queR = new LinkedList<>();
+        Queue<Integer> queD = new LinkedList<>();
 
-        for(int i = 0; i < n; i++) {
-            if(senate.charAt(i) == 'R') {
-                rad.add(i);
+        String[] arrStr = senate.split("");
+        int strLen = arrStr.length;
+
+        for(int i = 0; i < strLen; i++) {
+            if(arrStr[i].equals("R")) {
+                queR.offer(i);
             } else {
-                dir.add(i);
+                queD.offer(i);
             }
         }
 
-        while(!rad.isEmpty() && !dir.isEmpty()) {
-            if(rad.peek() < dir.peek()) {
-                rad.add(n++);
+        while(!queR.isEmpty() && !queD.isEmpty()) {
+            if(queR.peek() > queD.peek()) {
+                queR.poll();
+                queD.offer(queD.poll() + strLen);
             } else {
-                dir.add(n++);
+                queD.poll();
+                queR.offer(queR.poll() + strLen);
             }
-            rad.poll();
-            dir.poll();
         }
 
-        return rad.isEmpty() ? "Dire" : "Radiant";
+        return queD.isEmpty() ? "Radiant" : "Dire";
     }
 }
