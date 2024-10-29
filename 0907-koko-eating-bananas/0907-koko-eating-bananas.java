f3aafdf6619e55feaa814dec071680c0cbc3cce6
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-29 12:48:49 +0900

    Time: 30 ms (7.29%), Space: 44.9 MB (76.81%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,39 +1,34 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-         int max=Integer.MIN_VALUE;
-        for(int i=0;i<piles.length;i++)
-        {
-            max=Math.max(piles[i],max);
+        Arrays.sort(piles);
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < piles.length; i++) {
+            max = Math.max(piles[i], max);
         }
-        int low=1;
-        int high=max;
-        int a=-1;
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            int ans=ispos(piles,mid);
-            
-            if(ans<=h)
-            { 
-                a=mid;  
-                high=mid-1;
-            }
-            else
-            {
-                low=mid+1;
+        int low = 1;
+        int high = max;
+        int a = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int ans = ispos(piles, mid);
+
+            if (ans <= h) {
+                a = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return a;
-        
+
     }
-    public int ispos(int[] arr,int k)
-    {
-        int d=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            d+=Math.ceil((double)arr[i]/k);
-            
-            
+
+    public int ispos(int[] arr, int k) {
+        int d = 0;
+        for (int i = 0; i < arr.length; i++) {
+            d += Math.ceil((double) arr[i] / k);
+
         }
         return d;
     }
