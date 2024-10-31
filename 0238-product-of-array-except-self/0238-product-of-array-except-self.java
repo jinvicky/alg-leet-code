bac9a08a435fe964e5f4fae2aa160d3d8a2b6e5a
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-10-31 14:13:49 +0900

    Time: 1 ms (99.61%), Space: 55.3 MB (66.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,42 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] arr = new int[nums.length];
-        int zeroCount = 0;
+        int[] newArr = new int[nums.length];
 
-        arr[0] = nums[0];
-        if(nums[0] == 0) {
-            zeroCount++;
-            arr[0] = 1;
-        }
-        for (int i = 1; i < nums.length; i++) {
-
-            if (nums[i] == 0) {
-                zeroCount++;
-                arr[i] = arr[i - 1];
-
-            } else {
-                arr[i] = arr[i - 1] * nums[i];
-            }
+        int leftValue = 1;
+        for(int i = 0;i<nums.length;i++){
+            newArr[i] = leftValue;
+            leftValue*=nums[i];
         }
 
-        if (zeroCount > 1) {
-            return new int[nums.length];
-        } else if (zeroCount == 1) {
-            int[] result = new int[nums.length];
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == 0) {
-                    result[i] = arr[nums.length - 1];
-                }
-            }
-            return result;
-        } else {
-            // 0이 없는 경우
-            int[] result = new int[nums.length];
-            for (int i = 0; i < nums.length; i++) {
-                result[i] = arr[nums.length - 1] / nums[i];
-
-            }
-            return result;
+        int rightValue = 1;
+        for(int i = nums.length-1;i>=0;i--){
+            newArr[i]*=rightValue;
+            rightValue*= nums[i];
         }
+
+        return newArr;
     }
 }
+
+/*
+1 2 3 4
+1 1 2 6
+24  12  8  6 
+1 4 12 24 
+*/
