692c28b1ebecbaf07f7128f05dea1fd2ec68cb4e
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-11-02 20:09:20 +0900

    Time: 62 ms (69.69%), Space: 62.5 MB (42.98%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,19 +1,30 @@
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        Stack<List<Integer>> stack = new Stack<>();
-        int[] result = new int[temperatures.length];
-        for(int i = 0; i < temperatures.length; i++) {
-            int tmpt = temperatures[i];
+    // 내 정답
+    // public int[] dailyTemperatures(int[] temperatures) {
+    //     Stack<List<Integer>> stack = new Stack<>();
+    //     int[] result = new int[temperatures.length];
+    //     for (int i = 0; i < temperatures.length; i++) {
+    //         int tmpt = temperatures[i];
 
-            while(!stack.isEmpty() && stack.peek().get(0) < tmpt) {
-                // int peek = stack.peek().get(0);
-                // if(peek < tmpt) {
-                    List<Integer> pop = stack.pop();
-                    result[pop.get(1)] = i - pop.get(1); // 1-0 하면 30, 1 들어가겠지  
-                // }
+    //         while (!stack.isEmpty() && stack.peek().get(0) < tmpt) {
+    //             List<Integer> pop = stack.pop();
+    //             result[pop.get(1)] = i - pop.get(1); // 1-0 하면 30, 1 들어가겠지
+    //         }
+    //         stack.push(List.of(tmpt, i));
+    //     }
+    //     return result;
+    // }
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stk = new Stack<>();
+        int[] res = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(!stk.isEmpty() && temperatures[i] > temperatures[stk.peek()]) {
+                int index = stk.pop();
+                res[index] = i - index;
             }
-            stack.push(List.of(tmpt, i));
+            stk.push(i);
         }
-        return result;
+        return res;
     }
 }
