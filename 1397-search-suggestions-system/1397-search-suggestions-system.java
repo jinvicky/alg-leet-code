9f7cbddac7e100b6551ea992f82a041c7881a09a
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-11-06 15:40:10 +0900

    Time: 16 ms (81.21%), Space: 50.1 MB (22.78%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.java
+++ 1397-search-suggestions-system/1397-search-suggestions-system.java
@@ -1,45 +1,49 @@
 class Solution {
-        public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        //sort words so they will be added in a sorted order to nodes 
+   public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        // Sort products so they will be added in a sorted order to nodes 
         Arrays.sort(products);
         
-        Trie root = new Trie();
+        Trie<String> root = new Trie<>();
         for (String prod : products) {
-            Trie n = root;
+            Trie<String> n = root;
             for (char ch : prod.toCharArray()) {
                 int i = ch - 'a';
                 if (n.next[i] == null) {
-                    n.next[i] = new Trie();
+                    n.next[i] = new Trie<>();
                 }
                 n = n.next[i];
-                if (n.words.size() < 3)
+                if (n.words.size() < 3) {
                     n.words.add(prod);
+                }
             }
         }
         
-        List<List<String>> res = new ArrayList();
-        Trie n = root;
-		//start going over the search word char by char
+        List<List<String>> res = new ArrayList<>();
+        Trie<String> n = root;
+        // Start going over the search word char by char
         for (int i = 0; i < searchWord.length(); i++) {
             n = n.next[searchWord.charAt(i) - 'a'];
-			//if we met null - means no more matches possible, the result of result can be filled by empty lists
+            // If we met null, means no more matches possible
             if (n == null) {
-                for (int j = i; j < searchWord.length(); j++)
-                    res.add(Collections.EMPTY_LIST);    
+                for (int j = i; j < searchWord.length(); j++) {
+                    res.add(Collections.emptyList()); // Use emptyList() for type safety
+                }
                 break;
             }
-            res.add(n.words);
+            res.add(new ArrayList<>(n.words)); // Create a new list to avoid shared references
         }
         return res;
     }
-    
-}
-//trie node
-    class Trie {
-        Trie[] next;
-        List<String> words;
+
+    // Trie node
+    class Trie<T> {
+        Trie<T>[] next;
+        List<T> words;
+
+        @SuppressWarnings("unchecked")
         Trie() {
-            words = new ArrayList();
+            words = new ArrayList<>();
             next = new Trie[26];
         }
     }
+}
