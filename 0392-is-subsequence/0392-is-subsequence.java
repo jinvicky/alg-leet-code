6e571240ee143924924c9c75febee9f0e7508164
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-11-12 15:37:22 +0900

    Time: 1 ms (92.96%), Space: 41.4 MB (41.03%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,17 +1,22 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
+     public boolean isSubsequence(String s, String t) {
+        if(s.equals(t)) {
+            return true;
+        } else if (s.length() > t.length()) {
+            return false;
+        }
+        
         int pointer = 0;
         int range = s.length();
 
-        for (int i = 0; i < t.length(); i++) {
-            if (pointer >= range) {
+        for(int i = 0; i < t.length(); i++) {
+            if(pointer >= range) {
                 return true;
             }
-            if (s.charAt(pointer) == t.charAt(i)) {
+            if(s.charAt(pointer) == t.charAt(i)) {
                 pointer++;
             }
         }
-
         return pointer >= range;
     }
 }
