ae39ab93e0b4dfe02da456fe6c66e944b71d5488
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-11-18 13:36:43 +0900

    Time: 47 ms (81.34%), Space: 56.3 MB (77.36%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.java
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.java
@@ -1,31 +1,35 @@
 class Solution {
      public long totalCost(int[] costs, int k, int candidates) {
-        final Queue<Integer> left = new PriorityQueue<>();
-        final Queue<Integer> right = new PriorityQueue<>();
+        int n = costs.length;
+        int l = 0;
+        int r = n - 1;
 
-        final int n = costs.length;
-        int i = 0;
-        int j = n - 1;
+        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
 
-        long result = 0L;
-
-        while (k-- > 0) {
-            while (right.size() < candidates && i <= j) {
-                right.offer(costs[j--]);
+        for (int i = 0; i < candidates; i++) {
+            if (l <= r) {
+                pq1.add(costs[l++]);
             }
-
-            while (left.size() < candidates && i <= j) {
-                left.offer(costs[i++]);
-            }
-
-            if (!left.isEmpty() && !right.isEmpty()) {
-                result += left.peek() <= right.peek() ? left.poll() : right.poll();
-            } else if (!left.isEmpty()) {
-                result += left.poll();
-            } else {
-                result += right.poll();
+            if (l <= r) {
+                pq2.add(costs[r--]);
             }
         }
-        return result;
+        long cost = 0;
+
+        for(int i = 0; i < k; i++) {
+            if(!pq1.isEmpty() && (pq2.isEmpty() || pq1.peek() <= pq2.peek())) {
+                cost+=pq1.poll();
+                if(l<=r) {
+                    pq1.add(costs[l++]);
+                }
+            } else {
+                cost+=pq2.poll();
+                if(l<=r) {
+                    pq2.add(costs[r--]);
+                }
+            }
+        }
+        return cost;
     }
 }
