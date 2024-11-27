9f7ebda907337b7bb0a61b15709523791080b66b
jinvicky <73690018+jinvicky@users.noreply.github.com>
2024-11-27 17:46:12 +0900

    Time: 12 ms (98.89%), Space: 46.7 MB (25.83%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,15 +1,11 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-         nums = Arrays.stream(nums)
-                .boxed()
-                .sorted(Collections.reverseOrder())
-                .mapToInt(Integer::intValue)
-                .toArray();
 
+        Arrays.sort(nums);
         int maxSum = 0;
 
-        for(int i = 0; i < nums.length; i+=2) {
-            maxSum+= Math.min(nums[i], nums[i+1]);
+        for (int i = 0; i < nums.length; i += 2) {
+            maxSum += nums[i];
         }
 
         return maxSum;
