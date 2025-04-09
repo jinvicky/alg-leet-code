93458ecf37a7c8c88d6a2b9d0077e10ae42a96bd
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-04-09 14:25:27 +0900

    Time: 3 ms (22.64%), Space: 42 MB (97.18%) - LeetHub

--- 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
@@ -6,22 +6,16 @@ class Solution {
         }
 
         int minCost = 0;
-
         int index = cost.length - 1;
         int buyCnt = 0;
         while (index >= 0) {
-
             if (buyCnt < 2) {
                 minCost += cost[index];
-                System.out.println(minCost);
                 buyCnt++;
-                index -= 1;
             } else { // buyCnt == 2
-
-
-                index -= 1; // 무료 캔디 건너뛴 인덱스 설정
                 buyCnt = 0;
             }
+            index -= 1;
 
             if(index < 0) {
                 // 범위를 벗어난 경우 break;
