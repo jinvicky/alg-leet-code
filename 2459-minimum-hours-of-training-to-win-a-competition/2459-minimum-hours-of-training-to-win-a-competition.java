c873f3c32daced385fd5fd4458cc0f512ba67f27
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-04-10 15:23:00 +0900

    Time: 0 ms (100%), Space: 41.8 MB (86.99%) - LeetHub

--- 2459-minimum-hours-of-training-to-win-a-competition/2459-minimum-hours-of-training-to-win-a-competition.java
+++ 2459-minimum-hours-of-training-to-win-a-competition/2459-minimum-hours-of-training-to-win-a-competition.java
@@ -6,24 +6,16 @@ class Solution {
            egSum += e;
         }
         int egNeed = initialEnergy > egSum ? 0 : Math.abs(initialEnergy - egSum) + 1;
-
-        System.out.println("ans1 " +egNeed); // ok인듯
-
         int expNeed = 0;
 
         for(int exp : experience) {
             if(initialExperience <= exp) {
                 // 같거나 작다면? 
-
-                System.out.println(exp);
                 expNeed += exp - initialExperience + 1;
                 initialExperience += exp - initialExperience + 1;
             }
             initialExperience += exp;
         }
-
-        System.out.println("ans2 "+expNeed);
-
         return egNeed + expNeed;
     }
 }
