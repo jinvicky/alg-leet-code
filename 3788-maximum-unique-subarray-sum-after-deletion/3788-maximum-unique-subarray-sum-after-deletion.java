fcdd8c48f14712611b28bf1932cd098fbaafa0ee
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-04-28 10:03:35 +0900

    Time: 12 ms (5.05%), Space: 42.8 MB (33.12%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int maxSum(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int positiveSum = -90;
+        int negativeSum = 0;
+
+        int pMax = 0;
+        int nMax = Integer.MIN_VALUE;
+        for (int n : nums) {
+            if (set.add(n)) {
+                if (n < 0) {
+                    negativeSum += n;
+                    nMax = Math.max(nMax, n);
+                } else {
+                    positiveSum = positiveSum == -90 ? n : positiveSum + n;
+                    pMax = Math.max(pMax, n);
+                }
+            }
+        }
+
+        System.out.println(positiveSum + " and " + negativeSum);
+
+        // 오직 음수
+        if(positiveSum == -90) return nMax;
+        else return positiveSum;
+    }
+}
+
+// subarray가 중간 요소를 건너뛰어도 ok인데?
+// 그러면 음수만 모인 경우는? 
