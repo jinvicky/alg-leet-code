91bc84a9107bfa523011190dbb87bf42832ef381
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-05-07 11:09:23 +0900

    Time: 0 ms (100%), Space: 41.6 MB (56.16%) - LeetHub

--- 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.java
+++ 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.java
@@ -1,31 +1,17 @@
 class Solution {
     public int minTimeToType(String word) {
-        int clock = 0;
-        char prevC = 'a';
-        Map<Character, int[]> map = new HashMap<>();
+       int minCount = 0;
+       char from = 'a';
+       for(char c : word.toCharArray()) {
+            char to = c;
+            minCount += Math.min((to - from + 26) % 26, (from - to + 26) % 26);
+            minCount++;
+            from = c;
+       }
 
-        for (int i = 0; i < 26; i++) {
-            char c = (char) ('a' + i);
-            map.put(c, new int[] { i, i + 26 });
-        }
 
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i); // 알파벳 가지고 옴.
-
-            clock+=1; // 타이핑 시간도 추가
-            clock+= getMin(map.get(c), map.get(prevC));
-            prevC = c;
-        }
-        return clock;
+       return minCount;
     }
 
-    private int getMin (int[] a, int[]b) {
-        int min = Integer.MAX_VALUE;
-        for(int i : a) {
-            for(int j : b) {
-            min = Math.min(min, Math.abs(i - j)); // i와 j의 차이의 절대값을 계산
-            }
-        }
-        return min;
-    }
+
 }
