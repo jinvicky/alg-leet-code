5ad890ece41753e969ea68102bb347b4898817ae
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-05-09 14:03:29 +0900

    Time: 1 ms (97.5%), Space: 45.7 MB (60.15%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -4,7 +4,6 @@ class Solution {
         for(int i = 0; i < flowerbed.length; i++) {
             fbSum = (i==0 ? 0 : flowerbed[i-1]) + (i == flowerbed.length - 1 ? 0 : flowerbed[i+1]);
             if(fbSum < 1 && flowerbed[i] == 0) {
-                System.out.println(i);
                 flowerbed[i] = 1;
                 n--;
             }
