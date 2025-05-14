5b94061d5f2ca318b4943bf30d641ea9c5a94b81
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-05-14 13:19:02 +0900

    Time: 1 ms (99.61%), Space: 45.1 MB (95.75%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
@@ -1,16 +1,11 @@
 class Solution {
     public String largestOddNumber(String num) {
-        // 나올수 있는 홀수 중 가장 큰 문자열을 반환한다. 
-        char[] cArr = num.toCharArray();
-
-
-        for(int i = cArr.length - 1; i >= 0; i--) {
-            if(cArr[i] % 2 == 0) {
-                // 아무것도 하지 않는다. 
-            } else { // 홀수라면 기존 max값
+        for(int i = num.length() - 1; i>= 0; i--) {
+            char c = num.charAt(i);
+            if((c-'0') % 2 == 1) {
                 return num.substring(0, i+1);
             }
         }
-        return new String();
+        return "";
     }
 }
