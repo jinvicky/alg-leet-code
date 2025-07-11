3139cb1e0f603ae65e5e8611ed2808c1dab7044b
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-11 14:59:55 +0900

    Time: 0 ms (100%), Space: 40.1 MB (92.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] arr = new int[n];
+        if (n <= 3) return n;
+        int[] pibo = new int[n];
 
-        if(n < 3) {
-            switch(n) {
-                case 1: 
-                    return 1;
-                case 2:
-                    return 2;
-            }
+        pibo[0]=1;
+        pibo[1]=2;
+        pibo[2]=3;
+
+        for(int i = 3; i < n; i++) {
+            pibo[i] = pibo[i-1] + pibo[i-2];
         }
 
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
-
-        for(int i = 3; i < arr.length; i++) {
-            arr[i] = arr[i-1] + arr[i-2];
-        }
-
-        return arr[n-1];
+        return pibo[n-1];
     }
 }
