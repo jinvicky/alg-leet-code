652fbac501c1741a4c4a70a3f256eb5c5370baf9
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-11 13:53:59 +0900

    Time: 0 ms (100%), Space: 42.6 MB (59.24%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,31 +1,41 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-       ListNode dummy = new ListNode();
-       ListNode cur = dummy;
+        /**
+            결과값을 반환할 answer 노드와 머지를 진행할 dummy 노드, 총 2개의 노드가 필요하다.
+            오름차순 정렬인 것을 보아, 더 값이 작은 노드를 비교 후 추가해야 한다. 
+        
+            두 노드의 길이가 다르다면? -> 같을때까지만 하고 나머지는 그냥 다 붙여야겠는데?
+            왜냐하면 조건이 두 리스트는 각각은 "오름차순" 정렬되어 있거든.
+         */
 
-       while(list1 != null && list2 != null) {
-            if(list1.val > list2.val) {
-                cur.next = list2;
+        ListNode answer = new ListNode();
+        ListNode dummy = answer;
+
+        while(list1 != null && list2 != null) { // 두 노드가 모두 널이 아닐때까지
+            // 값을 비교한다. 
+            if (list1.val > list2.val) {
+                dummy.next = list2;
                 list2 = list2.next;
             } else {
-                cur.next = list1;
+                dummy.next = list1;
                 list1 = list1.next;
             }
-            cur = cur.next;
-       }
+            dummy = dummy.next;
+        }
 
-       cur.next = (list1 != null) ? list1 : list2;
+        // 둘중에 널이 아닌 노드를 dummy의 다음에 연결한다. 
+        dummy.next = (list1 != null) ? list1 : list2;
 
-       return dummy.next;
+        return answer.next;
     }
 }
