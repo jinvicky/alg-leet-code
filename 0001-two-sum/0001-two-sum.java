a7cb3bf37e52a237bd70e7fc95ecde2bf004e3f6
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-11 13:33:38 +0900

    Time: 44 ms (37.45%), Space: 45.2 MB (30.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
+        int[] answer = new int[2];
+        /** 
+            타겟값을 만족하는 두 요소의 인덱스를 배열에 담아 반환하라.  
+            정답 케이스는 단 1개다.       
+        */      
+
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++) {
                 if(nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
             }
         }
-        return new int[]{0,1};
+
+        return answer;
     }
 }
