809615f7f60610b4d2b62105b26b83f438d1f323
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-11 13:42:18 +0900

    Time: 2 ms (96.77%), Space: 42.3 MB (8.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,30 @@
 class Solution {
     public boolean isValid(String s) {
+        /**
+            스택이 비었다면 true, 차있다면 false다.
+            괄호들에 대해서는 case 문으로 정립한다. 
+         */
+
         Stack<Character> stack = new Stack<>();
 
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '{' || s.charAt(i) == '[' || s.charAt(i) == '(') {
-                stack.push(s.charAt(i));
+        for (char c : s.toCharArray()) {
+            // 좌측 괄호면 무조건 추가한다. 
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
             } else {
-                // } 또는 ] 또는 )인 경우
+                if(stack.isEmpty()) return false; // 우측 괄호가 있는데 스택이 비었으면 false 
                 char peek = stack.peek();
-
-                if (peek == '{' && s.charAt(i) == '}') {
+                if(peek == '(' && c  == ')') {
                     stack.pop();
-                } else if (peek == '[' && s.charAt(i) == ']') {
+                } else if (peek == '{' && c == '}') {
                     stack.pop();
-                } else if (peek == '(' && s.charAt(i) == ')') {
+                } else if (peek == '[' && c == ']') {
                     stack.pop();
                 } else {
-                    return false;
+                    stack.push(c);
                 }
             }
         }
-            return stack.isEmpty() ? true : false;
+        return stack.isEmpty();
     }
 }
