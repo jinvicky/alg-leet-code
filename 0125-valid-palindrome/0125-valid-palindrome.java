4b1ab3886a1d188e5426620ca871319bdc65daaa
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-11 15:33:05 +0900

    Time: 614 ms (5.19%), Space: 46.8 MB (5.68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,46 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // String에서 특수문자랑 공백을 제거한다.
-    s = s.replace("`", "")
-            .replace(";", "")
-            .replace("?", "")
-            .replace("!", "")
-            .replace(".", "")
-            .replace(",", "")
-            .replace(":", "")
-            .replace("@", "")
-            .replace("#", "")
-            .replace("_", "")
-            .replace("{", "")
-            .replace("}", "")
-            .replace("[", "")
-            .replace("]", "")
-            .replace("'", "")
-            .replace("\"", "")
-            .replace("(", "")
-            .replace(")", "")
-            .replace("-", "")
-            .replace("+", "")
-            .replace("&", "")
-            .replace("*", "")
-            .replace("/", "")
-            .replace("\\", "");
-
-        // " "의 경우는 그냥 true를 리턴한다. length가 1인데 " " 내용일 때 
-        if(s.length() == 1 && s.equals(" ")) {
-            return true;
+        /**
+           변환하고 바꾸면 늦는데.. 
+         */
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if(Character.isDigit(c)) {
+                sb.append(c);
+            }
+            if(Character.isAlphabetic(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
         }
 
-        s = s.replace(" ", "");
         int left = 0;
-        int right = s.length() - 1;
+        int right = sb.toString().length() -1;
 
         while (left <= right) {
-            char leftVal = Character.toLowerCase(s.charAt(left));
-            char rightVal = Character.toLowerCase(s.charAt(right));
-
-            // 대소문자 구분 없이 비교하려면 toLowerCase()를 사용할 수 있습니다.
-            if (leftVal != rightVal) {
+            System.out.println(sb.toString().charAt(left));
+            System.out.println(sb.toString().charAt(right));
+            if(sb.toString().charAt(left) != sb.toString().charAt(right)) {
                 return false;
             }
             left++;
