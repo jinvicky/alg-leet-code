0baab800a9d9a0449efc67be61493c4e462fd80a
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-14 14:16:09 +0900

    Time: 0 ms (100%), Space: 45.5 MB (43.17%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -5,18 +5,18 @@ class Solution {
             0부터 nums.length까지의 값들 중에서 없는 값을 반환하면 됩니다.
             단 O(n)으로 한다면 좋겠습니다. -> 그냥 O(n**2)만 피해도 굿
         
-        
-        
+            O(n) 성능으로 최적화한다.
+            0이 missing 자리에 들어가서 생긴 문제니 0 제외부터? -> 0이 맨 처음이 아니기 때문에 불가능.
          */
-        Set<Integer> set = new HashSet<>();
+        int arrayTotal = 0;
         for (int n : nums) {
-            set.add(n);
+            arrayTotal += n;
         }
-        for (int i = 0; i <= nums.length; i++) {
-            if (!set.contains(i)) {
-                return i;
-            }
+
+        int numberTotal = 0;
+        for (int i = 1; i <= nums.length; i++) {
+            numberTotal += i;
         }
-        return 0;
+        return numberTotal - arrayTotal;
     }
 }
