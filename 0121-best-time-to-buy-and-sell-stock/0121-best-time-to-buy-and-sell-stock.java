8a79d10a5b31c996de658b453cd66db58763dd82
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-14 15:52:29 +0900

    Time: 1306 ms (2.01%), Space: 59.1 MB (97.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int answer = 0;
-        int myStock = prices[0];
+        /**
+            얻을 수 있는 최대 이익의 값(요소 정보 없어도 됨)
+        
+         */
+        int minBuy = prices[0];
+        int[] max = new int[prices.length];
 
-        for(int i = 1; i < prices.length ;i++) {
-            if(myStock > prices[i]) { // 내 스톡이 더 크다.
-                myStock = prices[i]; // 더 작은 값으로 내 주식을 갱신
-            } else {
-                answer = Math.max(answer, Math.abs(myStock - prices[i]));
-            }
+        for (int i = 1; i < prices.length; i++) {
+            max[i] = Math.max(prices[i] - minBuy, max[i-1]);
+
+            // 다음 연산에 사용할 buy 기준값을 갱신.
+            minBuy = Math.min(prices[i], minBuy);
+
+            System.out.println(max[i]);
         }
-        return answer;
+
+        return max[prices.length-1];
     }
 }
