4f1c5de9b15fb1eaadcd5bfb4a1146dce1587761
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-15 17:34:09 +0900

    Time: 0 ms (100%), Space: 42.7 MB (61.76%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -1,41 +1,29 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
 
-    private int maxDp = 0;
-
-    public void calculate(TreeNode node, int dp) {
-        if(node == null) {
-            return;
-        }
-        dp+=1;
-        this.maxDp = Math.max(dp, maxDp);
-
-        System.out.println(maxDp+ " and " + node.val);
-        calculate(node.left, dp);
-        calculate(node.right, dp);
-    }
-
     public int maxDepth(TreeNode root) {
-        if(root == null) {
-            return 0;
-        } else if (root.left == null && root.right == null) {
-            return 1;
-        }
-        calculate(root, 0);
-        return maxDp;
+        /**
+            root가 널값이면 0을 반환한다. 
+            재귀를 left, right 방향으로 호출하고, return 0일 때 기존 재귀값들을 모두 더함 + 1을 반환한다. 
+         */
+
+         if(root==null) return 0;
+         return 1+ Math.max(maxDepth(root.left), maxDepth(root.right));
     }
+
+
 }
