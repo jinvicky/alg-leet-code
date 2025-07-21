80d56cb9e1892693c68b1770128fdb1cab51c351
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-21 12:32:45 +0900

    Time: 44 ms (37.81%), Space: 44.8 MB (89.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,20 @@
+/**
+    본래 brute force로 이중 for문으로 풀었다가 
+    map으로 최적화를 시도.
+ */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
-        /** 
-            타겟값을 만족하는 두 요소의 인덱스를 배열에 담아 반환하라.  
-            정답 케이스는 단 1개다.       
-        */      
-
-        int sum = 0;
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i+1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
                 }
             }
         }
-
-        return answer;
+        return new int[2];
     }
+
+    // public int[] twoSumByBruteForce(int[] nums, int target) {
+
+    // }
 }
