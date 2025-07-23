4f87a87afb0533a847457861981a1c9f5919fe30
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-23 14:02:52 +0900

    Time: 20 ms (11.25%), Space: 45.3 MB (6.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,32 +1,23 @@
 class Solution {
-    public int charToNumber(char c) {
-        return c - 'a'; // 'a'는 0, 'b'는 1, ..., 'z'는 25
-    }   
-
     public boolean isAnagram(String s, String t) {
+        TreeMap<Character, Integer> map = new TreeMap<>();
 
-        if(s.length() != t.length()) {
-            return false;
-        }
-        int[] sAlphas = new int[26];
-        int[] tAlphas = new int[26];
-
-        // 각 문자열의 문자를 카운트
-        for (int i = 0; i < s.length(); i++) {
-            sAlphas[charToNumber(s.charAt(i))]++;
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            tAlphas[charToNumber(t.charAt(i))]++;
+        for (char c : t.toCharArray()) {
+            Integer v = map.get(c);
+
+            if (v == null)
+                return false;
+
+            if (v - 1 == 0) {
+                map.remove(c);
+            } else
+                map.put(c, v - 1);
         }
 
-        // 두 배열 비교
-        for (int j = 0; j < 26; j++) {
-            if (sAlphas[j] != tAlphas[j]) {
-                return false; // 개수가 다르면 애너그램이 아님
-            }
-        }
-
-        return true; // 모든 문자의 개수가 같으면 애너그램
+        return map.size() < 1;
     }
 }
