3d321baa21ab8c07d43a2a7c28b77168df90a869
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-07-28 11:05:45 +0900

    Time: 3 ms (91.78%), Space: 44.7 MB (34.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        TreeMap<Character, Integer> map = new TreeMap<>();
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
 
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
 
-        for (char c : t.toCharArray()) {
-            Integer v = map.get(c);
-
-            if (v == null)
-                return false;
-
-            if (v - 1 == 0) {
-                map.remove(c);
-            } else
-                map.put(c, v - 1);
-        }
-
-        return map.size() < 1;
+        return new String(sChars).equals(new String(tChars));
     }
 }
