2ef752b1c4cea6e0f9e4adaae2ccfdc2465607b5
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-08-04 14:59:06 +0900

    Time: 1 ms (99.53%), Space: 57.6 MB (8.2%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,19 +1,19 @@
+import java.util.List;
+
 class Solution {
     public int maxSubArray(int[] nums) {
-        int res = nums[0];
-        int total = 0;
+        int currentMax = nums[0];
+        // 전체 최대 부분합을 추적하는 변수
+        int overallMax = nums[0];
 
-        for (int n : nums) {
-            if(total < 0) {
-                total = 0;
-            }
-
-            total += n;
-            res = Math.max(total, res);
-
+        for (int i = 1; i < nums.length; i++) {
+            // 현재 숫자가 더 큰지, 아니면 이전 부분합에 더한 값이 더 큰지 비교
+            currentMax = Math.max(nums[i], currentMax + nums[i]);
+            
+            // 전체 최대값 업데이트
+            overallMax = Math.max(overallMax, currentMax);
         }
 
-        return res;
-        
+        return overallMax;
     }
 }
