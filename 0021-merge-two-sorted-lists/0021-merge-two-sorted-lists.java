f3242101e84bd456b9258ffd8aa92b2294d1e7e0
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-08-11 09:56:24 +0900

    Time: 0 ms (100%), Space: 42.7 MB (45.52%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,40 +1,27 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        /**
-            결과값을 반환할 answer 노드와 머지를 진행할 dummy 노드, 총 2개의 노드가 필요하다.
-            오름차순 정렬인 것을 보아, 더 값이 작은 노드를 비교 후 추가해야 한다. 
-        
-            두 노드의 길이가 다르다면? -> 같을때까지만 하고 나머지는 그냥 다 붙여야겠는데?
-            왜냐하면 조건이 두 리스트는 각각은 "오름차순" 정렬되어 있거든.
-         */
-
         ListNode answer = new ListNode();
-        ListNode dummy = answer;
+        ListNode mergePointer = answer;
 
-        while(list1 != null && list2 != null) { // 두 노드가 모두 널이 아닐때까지
-            // 값을 비교한다. 
-            if (list1.val > list2.val) {
-                dummy.next = list2;
-                list2 = list2.next;
-            } else {
-                dummy.next = list1;
+        while (list1 != null && list2 != null) {
+            // list1의 값이 list2의 값보다 작거나 같으면 list1의 노드를 병합
+            if (list1.val <= list2.val) { // <-- 수정된 부분
+                mergePointer.next = list1;
                 list1 = list1.next;
+            } else {
+                // 그렇지 않으면 list2의 노드를 병합
+                mergePointer.next = list2;
+                list2 = list2.next;
             }
-            dummy = dummy.next;
+            mergePointer = mergePointer.next;
         }
 
-        // 둘중에 널이 아닌 노드를 dummy의 다음에 연결한다. 
-        dummy.next = (list1 != null) ? list1 : list2;
+        // 남은 노드들을 모두 병합
+        if (list1 != null) {
+            mergePointer.next = list1;
+        } else {
+            mergePointer.next = list2;
+        }
 
         return answer.next;
     }
