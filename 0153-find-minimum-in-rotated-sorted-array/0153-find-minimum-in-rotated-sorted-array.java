e52251a4ef301c4419463ffa48810028984d23cd
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-08-12 12:44:07 +0900

    Time: 0 ms (100%), Space: 42 MB (57.33%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,17 +1,29 @@
 class Solution {
     public int findMin(int[] nums) {
-        int start = 0;
-       int end = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
 
-       while(start < end) {
-           int mid = start + (end - start) / 2;
+        // 배열이 회전되지 않은 경우 (이미 정렬되어 있는 경우)
+        if (nums[right] >= nums[left]) {
+            return nums[left];
+        }
 
-           if (nums[mid] > nums[end]) {
-               start = mid + 1;
-           } else {
-               end = mid;
-           }
-       }
-       return nums[start];
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // mid가 right보다 크다면, 최소값은 mid의 오른쪽에 있다.
+            // 예를 들어 [3, 4, 5, 1, 2] -> mid=5. 최소값은 오른쪽에 있다.
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } 
+            // mid가 right보다 작다면, 최소값은 mid이거나 mid의 왼쪽에 있다.
+            // 예를 들어 [5, 1, 2, 3, 4] -> mid=2. 최소값은 mid이거나 왼쪽에 있다.
+            else {
+                right = mid;
+            }
+        }
+        
+        // 반복문이 끝나면 left와 right는 최소값을 가리킨다.
+        return nums[left];
     }
 }
