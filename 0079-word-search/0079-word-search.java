a143ef34f88699977faf22d7887e7c392bef4a2d
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-08-12 11:16:14 +0900

    Time: 270 ms (11.18%), Space: 41.4 MB (89.31%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        boolean result = false;        
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == word.charAt(0)) {
+                    result = backtrack(board, word, visited, i, j, 0);
+                    if(result) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+
+        if(index == word.length()) {
+            return true;
+        }
+
+        if(i<0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        visited[i][j] = true;
+
+        int up = i+1;
+        int down = i-1;
+        int right = j+1;
+        int left = j-1;
+
+        if(backtrack(board, word, visited, up, j, index+1)
+           || backtrack(board, word, visited, down, j, index+1)
+           || backtrack(board, word, visited, i, right, index+1)
+           || backtrack(board, word, visited, i, left, index+1)
+        ) {
+            return true;
+        }
+
+        visited[i][j] = false;
+        return false;
+    }
+    
+}
