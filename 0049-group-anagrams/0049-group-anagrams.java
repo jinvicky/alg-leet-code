13c5303236c062a11f8d46eb80bc442737f08fb8
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-08-18 10:16:09 +0900

    Time: 41 ms (7%), Space: 49.2 MB (8.62%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // 길이가 1이면 자체를 List로 감싸서 반환한다. 
+        if (strs.length == 1) {
+            return List.of(List.of(strs[0]));
+        }
+
+        List<List<String>> groupList = new ArrayList<>();
+        /**
+         * bf로 그룹핑을 할 수 있나?
+         * strs를 for문으로 돌면서 "정렬한" 단어가 map에 있는 지 확인하고 존재할 경우 key로 조회한 list에 추가한다.
+         * 마지막으로 map을 순회하면서 list들을 groupList.add(list); 한다. 
+         */
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            String sortedS = Arrays.stream(s.split(""))
+                    .sorted()
+                    .collect(Collectors.joining());
+
+            // System.out.println(sortedS);
+
+            List<String> v = map.get(sortedS);
+            if (v == null) {
+                map.put(sortedS, new ArrayList<>());
+            }
+            // System.out.println(map.get(sortedS));
+            map.get(sortedS).add(s);
+        }
+
+        for (List<String> list : map.values()) {
+            groupList.add(list);
+        }
+        return groupList;
+    }
+}
