17b92ee8c4d872f29da5d0fc1a3b73db3b93cd0d
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-08-21 10:36:12 +0900

    Time: 8 ms (42.21%), Space: 44.8 MB (29.9%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         int n = s.length();
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true; // 빈 문자열은 항상 가능
+        Set<String> wordSet = new HashSet<>(wordDict);
 
-        for (int i = 0; i < n; i++) {
-            if (!dp[i]) continue; // 여기까지 못 오면 확장 불가
-            for (String w : wordDict) {
-                int j = i + w.length();
-                if (j <= n && s.startsWith(w, i)) {
-                    dp[j] = true;
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
                 }
             }
         }
