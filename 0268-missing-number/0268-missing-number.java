bcb370983883d3b40c485d3e6b709f67e13cc9a3
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-08-25 16:43:16 +0900

    Time: 8 ms (5.57%), Space: 46 MB (7.17%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,22 +1,17 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        /**
-            n은 nums.length와 같습니다. 
-            0부터 nums.length까지의 값들 중에서 없는 값을 반환하면 됩니다.
-            단 O(n)으로 한다면 좋겠습니다. -> 그냥 O(n**2)만 피해도 굿
-        
-            O(n) 성능으로 최적화한다.
-            0이 missing 자리에 들어가서 생긴 문제니 0 제외부터? -> 0이 맨 처음이 아니기 때문에 불가능.
-         */
-        int arrayTotal = 0;
+        int expected = 0; // 0부터 n까지 더한 숫자의 합계
+        int input = 0; //nums가 준 숫자들의 합계
+
         for (int n : nums) {
-            arrayTotal += n;
+            input += n;
         }
 
-        int numberTotal = 0;
-        for (int i = 1; i <= nums.length; i++) {
-            numberTotal += i;
+        for (int i = 0; i <= nums.length; i++) {
+            expected += i;
         }
-        return numberTotal - arrayTotal;
+        System.out.println(expected + " and " + input);
+
+        return expected - input;
     }
 }
