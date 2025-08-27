1795b8a2db0285d49b640ba07c7ea7dbe2a9e7fa
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-08-27 16:04:01 +0900

    Time: 1 ms (88.63%), Space: 42.2 MB (31.07%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
@@ -1,28 +1,17 @@
 class Solution {
     public int countGoodSubstrings(String s) {
         // map으로 짠다음에 ch 중복이 없으면 map의 val을 더하자. 
-        Map<String, Integer> map = new HashMap<>();
         int offset = 3;
-        for(int i =0; i < s.length(); i++) {
-            if(i+offset > s.length()) break;
-            String parse = s.substring(i, i+offset);
-            map.put(parse, map.getOrDefault(parse, 0) +1);
-        }
-
-        // System.out.println(map);
-
         int max = 0;
-        // map을 순회해서 문자열 내 중복을 체크해서 없으면 maxCount++한다. 
-        for(Map.Entry<String, Integer> entry: map.entrySet()) {
-            String str = entry.getKey();
-            int v = entry.getValue();
+        for (int i = 0; i < s.length(); i++) {
+            if (i + offset > s.length())
+                break;
+            String str = s.substring(i, i + offset);
 
-            // System.out.println(str.charAt(0) + " and " + str.charAt(1) + " and " + str.charAt(2));
-            if(str.charAt(0) != str.charAt(1) && str.charAt(1) != str.charAt(2) && str.charAt(0) != str.charAt(2)) {
-                max += v;
+            if (str.charAt(0) != str.charAt(1) && str.charAt(1) != str.charAt(2) && str.charAt(0) != str.charAt(2)) {
+                max ++;
             }
         }
-
         return max;
     }
 }
