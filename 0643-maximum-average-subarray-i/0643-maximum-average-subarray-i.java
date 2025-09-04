d31027b59f3b677cae797128792996b99650b1d6
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-09-04 14:20:18 +0900

    Time: 5 ms (47.08%), Space: 56.7 MB (24.56%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,38 +1,28 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        double maxAvg = 0;
-        double sum = 0;
+        // end - start == k-1 로 간격 계산
 
-        for(int j = 0; j < k; j++) {
-            sum += nums[j];
+        double maxAvg = 0.0;
+        double currentAvg = 0.0;
+
+        for (int i = 0; i < k; i++) {
+            maxAvg += nums[i]; // 맨 처음 k개 window 합
         }
 
-        maxAvg = average(sum, k);
+        // 현재 평균을 초기값으로 설정
+        currentAvg = maxAvg;
+        // 최대 평균의 초기값
+        maxAvg = maxAvg / k;
 
-        for(int i = k; i < nums.length; i++) {
-            sum += nums[i]; // 현재 인덱스의 요소 추가
-            sum -= nums[i - k]; // k 이전 인덱스의 요소 제거
+        int slide = 0;
+        for (int i = k; i < nums.length; i++) {
+            currentAvg += nums[i];
+            currentAvg -= nums[slide];
 
-            double avgCalc = average(sum, k);
-            if(avgCalc > maxAvg) {
-                maxAvg = avgCalc; // 최대 평균 업데이트
-            }
+            maxAvg = Math.max(maxAvg, currentAvg / k);
+            slide++;
         }
-        return Math.round(maxAvg * 100000) / 100000.0;
 
-        // for(int i = 1; i < nums.length; i++) {
-        //     sum += nums[i+k]; // 이게 아니라 i + k 든가 그 식으로 가야함
-        //     double avgCalc = average(sum, k);
-
-        //     if(avgCalc > maxAvg) {
-        //         maxAvg = avgCalc;
-        //     }
-        //     sum -= nums[i];
-        // }
-        // return Math.round(maxAvg * 100000) / 100000.0;
-    }
-
-    public double average(double sum, int k) {
-        return sum / k;
+        return maxAvg;
     }
 }
