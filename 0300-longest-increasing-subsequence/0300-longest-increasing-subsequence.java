9ad393f6b72b836f7559b84de22dd3b94734ba70
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-10-04 16:35:10 +0900

    Time: 41 ms (60.96%), Space: 44.2 MB (68.38%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        List<Integer> tails = new ArrayList<>(); // 길이 k인 증가 수열의 "꼬리 최솟값"들
+        // null이거나 길이가 0일 때 사전 브레이크 
+        if (nums == null || nums.length == 0)
+            return 0;
 
-        for (int x : nums) {
-            int idx = Collections.binarySearch(tails, x);
-            if (idx < 0) idx = -(idx + 1);   // ★ 음수면 삽입 위치로 변환 (lower_bound)
+        int answer = 0;
+        int[] dp = new int[nums.length];
 
-            if (idx == tails.size()) {
-                tails.add(x);                // 가장 크면 뒤에 추가 → 길이 +1
-            } else {
-                tails.set(idx, x);           // 아니면 그 자리의 꼬리를 더 작은 x로 교체
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+ 1);
+                }
             }
+            // 5. 전체 LIS 길이 갱신
+            answer = Math.max(answer, dp[i]);
         }
-        return tails.size(); // 꼬리 배열 길이 = LIS 길이
+        return answer;
     }
 }
